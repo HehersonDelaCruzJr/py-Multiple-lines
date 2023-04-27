4e8cb281e823865bc6e1251f249ac9557dc84b06
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-04-28 02:28:31 +0800

    Completion of code, Ask the user if there are more line to add and put the lines to a file named 'multiplelines.txt'

--- MultipleLines.py
+++ MultipleLines.py
@@ -1,4 +1,11 @@
 # Open the file for writing
-with open('mylife.txt', 'w') as file:
-    #Ask for a line and put it in a file
-    file.write(input('Enter Line: '))
+with open('multiplelines.txt', 'w') as file:
+    
+# Ask the user to enter the line and put it into a file
+    while True:
+        line = input ("Enter the line: ")
+        file.write(line + "\n")
+#Ask the user if there are more lines to enter
+        more_lines = input("Are there more lines y/n? ")
+        if more_lines.lower() == 'n':
+            break
